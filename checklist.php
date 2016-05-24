7b35b14af73839c165067a5c467927062b610714
talesbaz <talesbaz@gmail.com>
2016-05-23 21:57:10 -0300

    ajustado permissao e tamanho do brasao

--- checklist.php
+++ checklist.php
@@ -24,7 +24,7 @@
   <div class="section">
 
     <div class="row">
-      <div class="col s3 center-align" style="padding-top: 19px;"><img src="images/internacional.png" width="95" height="95" /><h6>Sport Club Internacional</h6></div>
+      <div class="col s3 center-align" style="padding-top: 19px;"><img src="images/internacional.png" width="75" height="75" /><h6>Sport Club Internacional</h6></div>
       <div class="col s6 center-align">
         <h4>&nbsp;</h4>
         <p><b>Domingo, 15.04.2016, 17:00 Hr</b><br/>Estádio Beira Rio, Av. Padre Cacique, 891<br/>Bairro Praia de Belas</p>
