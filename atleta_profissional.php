cf9366d9774e439927889d891908bc7ad00ff8ab
talesbaz <talesbaz@gmail.com>
2016-05-27 01:10:59 -0300

    adicionado menu de atletas

---
+++ atleta_profissional.php
@@ -0,0 +1,109 @@
+<!--breadcrumbs start-->
+<div id="breadcrumbs-wrapper">
+    <!-- Search for small screen -->
+    <div class="header-search-wrapper grey hide-on-large-only">
+        <i class="mdi-action-search active"></i>
+        <input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Buscar">
+    </div>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m12 l12">
+        <h5 class="breadcrumbs-title">Atleta</h5>
+        <ol class="breadcrumbs">
+            <li><a href="index.php">Início</a></li>
+            <li><a href="#">Atleta</a></li>
+            <li class="active">Profissional</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="section">
+
+     <div class="section">
+
+      <div class="card-panel grey lighten-5 z-depth-1 hoverable">
+        <div class="row valign-wrapper">
+
+          <div class="col s4 m2">
+            <img src="http://www.internacional.com.br/jogadorescolorados/foto/Valdivia_GRUPO2016.jpg" alt="" class="circle responsive-img valign">
+          </div>
+
+          <div class="col s8 m10">
+            <ul class="collection with-header small">
+              <li class="collection-header">
+                <h4><span class="materialize-red-text text-darken-3">29</span> Valdívia</h4>
+              </li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Nome Completo:</span><br/> Wanderson Ferreira de Oliveira</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Data de Nascimento:</span><br/> 4 de outubro de 1994 (21 anos)</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Local de Nascimento:</span><br/> Jaciara (MT), Brasil</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Nacionalidade:</span><br/> Brasileira</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Altura:</span><br/> 1,74 m</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Pé:</span><br/> Destro</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Médico:</span><br/>
+              <blockquote><span class="materialize-red-text text-darken-4">Físico</span> - Estrutura óssea frágil<br/>
+              <span class="materialize-red-text text-darken-4">Odontológico</span> - Nunca teve problemas dentais<br/>
+              <span class="materialize-red-text text-darken-4">Nutricional</span> - Dieta de 2.000 cálorias<br/>
+              <span class="materialize-red-text text-darken-4">Psicológico</span> - Bom de grupo</blockquote>
+              </li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Jurídico:</span><br/>
+              <blockquote><span class="materialize-red-text text-darken-4">Partes</span> - 80% clube, 20% atleta<br/>
+              <span class="materialize-red-text text-darken-4">Tempo de Contrato</span> - 01/01/2016 à 29/06/2019<br/>
+              <span class="materialize-red-text text-darken-4">Salário</span> - R$ 400.000,00<br/>
+              <span class="materialize-red-text text-darken-4">Valor Nacional</span> - R$ 14 Milhões<br/>
+              <span class="materialize-red-text text-darken-4">Valor Internacional</span> - R$ 25 Milhões</blockquote>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+    <div class="section">
+
+      <div class="card-panel grey lighten-5 z-depth-1 hoverable">
+        <div class="row valign-wrapper">
+
+          <div class="col s4 m2">
+            <img src="http://www.internacional.com.br/jogadorescolorados/foto/DaniloFernandes_GRUPO2016.jpg" alt="" class="circle responsive-img valign">
+          </div>
+
+          <div class="col s8 m10">
+            <ul class="collection with-header small">
+              <li class="collection-header">
+                <h4><span class="materialize-red-text text-darken-3">1</span> Danilo Fernandes</h4>
+              </li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Nome Completo:</span><br/> Danilo Fernandes Batista</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Data de Nascimento:</span><br/> 3 de abril de 1988 (28 anos)</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Local de Nascimento:</span><br/> Guarulhos (SP), Brasil</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Nacionalidade:</span><br/> Brasileira</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Altura:</span><br/> 1,88 m</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Pé:</span><br/> Destro</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Médico:</span><br/>
+              <blockquote><span class="materialize-red-text text-darken-4">Físico</span> - Estrutura óssea razoável<br/>
+              <span class="materialize-red-text text-darken-4">Odontológico</span> - Nunca teve problemas dentais<br/>
+              <span class="materialize-red-text text-darken-4">Nutricional</span> - Alérgico à Gluten, dieta de 3.500 cálorias<br/>
+              <span class="materialize-red-text text-darken-4">Psicológico</span> - Bom de grupo, muito ligado à família</blockquote>
+              </li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Jurídico:</span><br/>
+              <blockquote><span class="materialize-red-text text-darken-4">Partes</span> - 70% clube, 10% atleta, 20 % Grupo Sonda<br/>
+              <span class="materialize-red-text text-darken-4">Tempo de Contrato</span> - 03/03/2016 à 02/03/2018<br/>
+              <span class="materialize-red-text text-darken-4">Salário</span> - R$ 150.000,00<br/>
+              <span class="materialize-red-text text-darken-4">Valor Nacional</span> - R$ 3 Milhões<br/>
+              <span class="materialize-red-text text-darken-4">Valor Internacional</span> - R$ 10 Milhões</blockquote>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+
+  </div>
+</div>
