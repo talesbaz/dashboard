01b0e33d526594d2c8726b33382658fbd9c6bb8d
talesbaz <talesbaz@gmail.com>
2016-05-27 17:46:11 -0300

    template sensores

---
+++ sensores.php
@@ -0,0 +1,26 @@
+<!--breadcrumbs start-->
+<div id="breadcrumbs-wrapper">
+    <!-- Search for small screen -->
+    <div class="header-search-wrapper grey hide-on-large-only">
+        <i class="mdi-action-search active"></i>
+        <input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Buscar">
+    </div>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m12 l12">
+        <h5 class="breadcrumbs-title">Sensores de Treinamento</h5>
+        <ol class="breadcrumbs">
+            <li><a href="index.php">Início</a></li>
+            <li><a href="#">Alta Performance</a></li>
+            <li class="active">Sensores de Treinamento</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="section">
+
+  </div>
+</div>
