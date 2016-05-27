e9c2f2a4ba8af357d51ba6bae3af0a316b0b2863
talesbaz <talesbaz@gmail.com>
2016-05-27 17:45:46 -0300

    adicionado juridico

---
+++ juridico_profissional.php
@@ -0,0 +1,90 @@
+<!--breadcrumbs start-->
+<div id="breadcrumbs-wrapper">
+    <!-- Search for small screen -->
+    <div class="header-search-wrapper grey hide-on-large-only">
+        <i class="mdi-action-search active"></i>
+        <input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Buscar">
+    </div>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m12 l12">
+        <h5 class="breadcrumbs-title">Jurídico</h5>
+        <ol class="breadcrumbs">
+            <li><a href="index.php">Início</a></li>
+            <li><a href="#">Jurídico</a></li>
+            <li class="active">Profissional</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="section">
+
+     <div class="section">
+
+      <div class="card-panel grey lighten-5 z-depth-1 hoverable">
+        <div class="row valign-wrapper">
+
+          <div class="col s4 m2">
+            <img src="http://www.internacional.com.br/jogadorescolorados/foto/Valdivia_GRUPO2016.jpg" alt="" class="circle responsive-img valign">
+          </div>
+
+          <div class="col s8 m10">
+            <ul class="collection with-header small">
+              <li class="collection-header">
+                <h4><span class="materialize-red-text text-darken-3">29</span> Valdívia</h4>
+              </li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Nome Completo:</span><br/> Wanderson Ferreira de Oliveira</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Partes:</span><br/>80% clube, 20% atleta</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Tempo de Contrato:</span><br/>01/01/2016 à 29/06/2019</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Salário:</span><br/>R$ 400.000,00</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Valor Nacional:</span><br/> R$ 14 Milhões</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Valor Internacional:</span><br/>R$ 25 Milhões</li>
+              <li class="collection-item"><button class="btn red darken-4 waves-effect waves-light" onclick="return false;">Anexar Contrato <i class="mdi-av-my-library-books right"></i></button></li>
+            </ul>
+            <div class="section right">
+              <button class="btn red darken-4 waves-effect waves-light" onclick="return false;">Anexar Contrato <i class="mdi-av-my-library-books right"></i></button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+    <div class="section">
+
+      <div class="card-panel grey lighten-5 z-depth-1 hoverable">
+        <div class="row valign-wrapper">
+
+          <div class="col s4 m2">
+            <img src="http://www.internacional.com.br/jogadorescolorados/foto/DaniloFernandes_GRUPO2016.jpg" alt="" class="circle responsive-img valign">
+          </div>
+
+          <div class="col s8 m10">
+            <ul class="collection with-header small">
+              <li class="collection-header">
+                <h4><span class="materialize-red-text text-darken-3">1</span> Danilo Fernandes</h4>
+              </li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Nome Completo:</span><br/> Danilo Fernandes Batista</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Partes:</span><br/> 70% clube, 10% atleta, 20 % Grupo Sonda</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Tempo de Contrato:</span><br/> 03/03/2016 à 02/03/2018</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Salário:</span><br/>R$ 150.000,00</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Valor Nacional:</span><br/>R$ 3 Milhões</li>
+              <li class="collection-item"><span class="materialize-red-text text-darken-4">Valor Internacional:</span><br/>R$ 10 Milhões</li>
+            </ul>
+            <div class="section right">
+              <button class="btn red darken-4 waves-effect waves-light" onclick="return false;">Anexar Contrato <i class="mdi-av-my-library-books right"></i></button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+
+  </div>
+</div>
