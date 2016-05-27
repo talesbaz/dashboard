17ae5e12100830695e744afb95a7c2797b0ebaf7
talesbaz <talesbaz@gmail.com>
2016-05-27 17:47:25 -0300

    organizado itens

--- index.php
+++ index.php
@@ -183,11 +183,12 @@
                 </li>
                 <li class="no-padding">
                     <ul class="collapsible collapsible-accordion">
-                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="fa fa-users"></i> Comissão Técnica</a>
+                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="fa fa-bolt"></i> Alta Performance</a>
                             <div class="collapsible-body">
                                 <ul>
-                                    <li><a href="comissao_amador">Amador</a></li>
-                                    <li><a href="comissao_profissional">Profissional</a></li>
+                                    <li><a href="analise_video">Análise de Vídeo</a></li>
+                                    <li><a href="gps">GPS</a></li>
+                                    <li><a href="sensores">Sens. de Treinamento</a></li>
                                 </ul>
                             </div>
                         </li>
@@ -207,6 +208,18 @@
                 </li>
                 <li class="no-padding">
                     <ul class="collapsible collapsible-accordion">
+                        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="fa fa-users"></i> Comissão Técnica</a>
+                            <div class="collapsible-body">
+                                <ul>
+                                    <li><a href="comissao_amador">Amador</a></li>
+                                    <li><a href="comissao_profissional">Profissional</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li class="no-padding">
+                    <ul class="collapsible collapsible-accordion">
                         <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="fa fa-wrench"></i> Ferram. Administrativas</a>
                             <div class="collapsible-body">
                                 <ul>
@@ -217,8 +230,6 @@
                         </li>
                     </ul>
                 </li>
-                <li class="bold"><a href="marketing" class="waves-effect waves-cyan"><i class="fa fa-trademark"></i> Marketing</a></li>
-                <li class="bold"><a href="socio_torcedor" class="waves-effect waves-cyan"><i class="fa fa-child"></i> Sócio Torcedor</a></li>
                 <li class="no-padding">
                     <ul class="collapsible collapsible-accordion">
                         <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="fa fa-balance-scale"></i> Jurídico</a>
@@ -231,8 +242,8 @@
                         </li>
                     </ul>
                 </li>
-                <li class="bold"><a href="#" class="waves-effect waves-cyan"><i class="fa fa-heartbeat"></i> Agenda</a></li>
-                <li class="bold"><a href="#" class="waves-effect waves-cyan"><i class="fa fa-area-chart"></i> Relatórios</a></li>
+                <li class="bold"><a href="marketing" class="waves-effect waves-cyan"><i class="fa fa-trademark"></i> Marketing</a></li>
+                <li class="bold"><a href="socio_torcedor" class="waves-effect waves-cyan"><i class="fa fa-child"></i> Sócio Torcedor</a></li>
                 <li class="no-padding">
 
                     <?php
