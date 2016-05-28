31558940b29f3cfcdd9a67f246767c4eb36ed3fa
talesbaz <talesbaz@gmail.com>
2016-05-28 19:20:25 -0300

    adicionado estrutura para perfis e menu estadio

---
+++ estadios.php
@@ -0,0 +1,82 @@
+<!--breadcrumbs start-->
+<div id="breadcrumbs-wrapper">
+    <!-- Search for small screen -->
+    <div class="header-search-wrapper grey hide-on-large-only">
+        <i class="mdi-action-search active"></i>
+        <input type="text" name="Search" class="header-search-input z-depth-2" placeholder="Buscar">
+    </div>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m12 l12">
+        <h5 class="breadcrumbs-title">Estádios</h5>
+        <ol class="breadcrumbs">
+            <li><a href="dashboard.php">Início</a></li>
+            <li class="active">Estádios</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="section">
+        <div class="row">
+          <div class="col s12 m8 l9">
+                <div class="card">
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img class="activator" src="images/estadio_internacional.jpg" alt="Beira-Rio"/>
+                  </div>
+                  <div class="card-content">
+                    <span class="card-title activator grey-text text-darken-4">Estádio Beira-Rio <i class="mdi-navigation-more-vert right"></i></span>
+                  </div>
+                  <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">Estádio Beira-Rio<i class="mdi-navigation-close right"></i></span>
+                    <p style="text-align:justify">O Estádio José Pinheiro Borda, conhecido como Gigante da Beira-Rio ou simplesmente Beira-Rio, é um estádio de futebol pertencente ao Internacional, está localizado às margens do lago Guaíba, situado em Porto Alegre, no estado brasileiro do Rio Grande do Sul.</p>
+                    <ul class="collection">
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Nome:</span><br/> Estádio José Pinheiro Borda</li>
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Gramado:</span><br/> Grama natural (105 x 68 m)</li>
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Capacidade:</span><br/> 50.128</li>
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Público Recorde:</span><br/> 106.554</li>
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Primeiro Gol:</span><br/> Claudiomiro (S. C. Internacional)</li>
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Avaliação:</span><br/>
+                      <blockquote><span class="materialize-red-text text-darken-4">Gramado</span> - Excelente<br/>
+                      <span class="materialize-red-text text-darken-4">Iluminação</span> - Excelente<br/>
+                      <span class="materialize-red-text text-darken-4">Sala de Imprensa</span> - Ótimo<br/>
+                      <span class="materialize-red-text text-darken-4">Vestiários</span> - Ótimo</blockquote>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col s12 m8 l9">
+                <div class="card">
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img class="activator" src="images/estadio_gremio.jpg" alt="Beira-Rio"/>
+                  </div>
+                  <div class="card-content">
+                    <span class="card-title activator grey-text text-darken-4">Arena do Grêmio<i class="mdi-navigation-more-vert right"></i></span>
+                  </div>
+                  <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">Arena do Grêmio<i class="mdi-navigation-close right"></i></span>
+                    <p style="text-align:justify">Foi inaugurada em 8 de dezembro de 2012, com um show dos artistas americanos do Blue Man Group e uma partida amistosa contra o clube alemão Hamburgo, vencida pelo Grêmio pelo placar de 2 a 1, repetindo o resultado da final de 1983, quando o clube gaúcho conquistou o Campeonato Interclubes no Japão.</p>
+                    <ul class="collection">
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Nome:</span><br/> Arena OAS</li>
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Gramado:</span><br/> Grama natural (Lolium Perenne e Desso GrassMaster) (105 x 68 m)</li>
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Capacidade:</span><br/> 50.662</li>
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Público Recorde:</span><br/> 48.204</li>
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Primeiro Gol:</span><br/> André Lima (Grêmio)</li>
+                      <li class="collection-item"><span class="materialize-red-text text-darken-4">Avaliação:</span><br/>
+                      <blockquote><span class="materialize-red-text text-darken-4">Gramado</span> - Excelente<br/>
+                      <span class="materialize-red-text text-darken-4">Iluminação</span> - Excelente<br/>
+                      <span class="materialize-red-text text-darken-4">Sala de Imprensa</span> - Bom<br/>
+                      <span class="materialize-red-text text-darken-4">Vestiários</span> - Bom</blockquote>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+          </div>
+        </div>
+    </div>
+  </div>
---
+++ menu_clube.php
@@ -0,0 +1,75 @@
+<li class="no-padding">
+    <ul class="collapsible collapsible-accordion">
+        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="fa fa-futbol-o"></i> Atleta</a>
+            <div class="collapsible-body">
+                <ul>
+                    <li><a href="atleta_amador">Amador</a></li>
+                    <li><a href="atleta_profissional">Profissional</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</li>
+<li class="no-padding">
+    <ul class="collapsible collapsible-accordion">
+        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="fa fa-bolt"></i> Alta Performance</a>
+            <div class="collapsible-body">
+                <ul>
+                    <li><a href="analise_video">Análise de Vídeo</a></li>
+                    <li><a href="gps">GPS</a></li>
+                    <li><a href="sensores">Sens. de Treinamento</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</li>
+<li class="no-padding">
+    <ul class="collapsible collapsible-accordion">
+        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="fa fa-bar-chart"></i> Análise de Desempenho</a>
+            <div class="collapsible-body">
+                <ul>
+                    <li><a href="desempenho_amador">Amador</a></li>
+                    <li><a href="desempenho_profissional">Profissional</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</li>
+<li class="no-padding">
+    <ul class="collapsible collapsible-accordion">
+        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="fa fa-users"></i> Comissão Técnica</a>
+            <div class="collapsible-body">
+                <ul>
+                    <li><a href="comissao_amador">Amador</a></li>
+                    <li><a href="comissao_profissional">Profissional</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</li>
+<li class="no-padding">
+    <ul class="collapsible collapsible-accordion">
+        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="fa fa-wrench"></i> Ferram. Administrativas</a>
+            <div class="collapsible-body">
+                <ul>
+                    <li><a href="administrativas_amador">Amador</a></li>
+                    <li><a href="administrativas_profissional">Profissional</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</li>
+<li class="no-padding">
+    <ul class="collapsible collapsible-accordion">
+        <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="fa fa-balance-scale"></i> Jurídico</a>
+            <div class="collapsible-body">
+                <ul>
+                    <li><a href="juridico_amador">Amador</a></li>
+                    <li><a href="juridico_profissional">Profissional</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+</li>
+<li class="bold"><a href="marketing" class="waves-effect waves-cyan"><i class="fa fa-trademark"></i> Marketing</a></li>
+<li class="bold"><a href="socio_torcedor" class="waves-effect waves-cyan"><i class="fa fa-child"></i> Sócio Torcedor</a></li>
